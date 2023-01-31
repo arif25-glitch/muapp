e64511ae5372f73330ce82ba8695b3d6edce92af
Arif Nur <nnarif37@gmail.com>
2023-02-01 06:40:35 +0700

    new api added

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,18 @@ const route = require('express').Router();
 const {controller} = require('../controller/route_controller');
 
 // routing for music
-route.get("/music-play", controller.music.play);
-route.get("/music-read", controller.music.read);
-route.get("/music-list", controller.music.list);
-route.get("/music-read-image", controller.music.readImage);
+route.get("/api/music-play", controller.music.play);
+route.get("/api/music-read", controller.music.read);
+route.get("/api/music-list", controller.music.list);
+route.get("/api/music-read-image", controller.music.readImage);
 
 // routing for user
-route.get("/user-read-all", controller.user.read_all);
-route.get("/user-playlist", controller.user.playlist);
-route.post("/user-create", controller.user.crud.create);
-route.post("/user-read", controller.user.crud.read);
-route.post("/user-update", controller.user.crud.update);
-route.post("/user-delete", controller.user.crud.delete);
+route.get("/api/user-read-all", controller.user.read_all);
+route.get("/api/user-playlist", controller.user.playlist);
+route.post("/api/user-create", controller.user.crud.create);
+route.post("/api/user-read", controller.user.crud.read);
+route.post("/api/user-update", controller.user.crud.update);
+route.post("/api/user-delete", controller.user.crud.delete);
 
 // routing for admin
 route.post("/admin-music-create", controller.admin.crud.music.create);
